a07f85f8fa479c30aefb1270f25de26c91862e88
Ryan Urbanski <ryanurbanski9@gmail.com>
2022-02-03 18:40:16 -0500

    Assigning elements in main.js to an object is working.

--- main.js
+++ main.js
@@ -1,2 +1,25 @@
 
 console.log('javascript is connected and running!!')
+
+// Define the workings strings I'll need to compute and display
+// the calculations
+let workingString = ""
+let displayString = ""
+
+let displayObject = document.getElementById("display-box-text")
+console.log(displayObject)                                           // show whole object
+console.log(displayObject.innerHTML)                                  //Show the current text of this Object
+
+//change the string for display object
+displayObject.innerHTML = "New string is working!!"
+
+// set an eventlistener for the buttons
+let oneBox = document.getElementById('1-box')                         // couldn't get querySelector() to work, but this got it done
+console.log(oneBox)
+console.log(oneBox.innerHTML)
+
+
+
+
+
+

d69344e7fd44bd5a91ad407578d53d7de2381fe4
Ryan Urbanski <ryanurbanski9@gmail.com>
2022-02-03 19:27:02 -0500

    All event listeners working for this 'mini' calculator.

--- main.js
+++ main.js
@@ -13,7 +13,8 @@ console.log(displayObject.innerHTML)                                  //Show the
 //change the string for display object
 displayObject.innerHTML = "New string is working!!"
 
-// BUTTON EVENT LISTENERS
+// BUTTON EVENT LISTENERS ------------------------------------------------------------------------------
+// 1
 let oneBox = document.getElementById('1-box')                           // assign a value for '1-box' 
 console.log(oneBox)
 console.log(oneBox.innerHTML)
@@ -24,6 +25,7 @@ oneBox.addEventListener('click', function() {
     console.log('The value of working string is: ' + workingString)     
 })
 
+// 2
 let twoBox = document.getElementById('2-box')                            // assign a value for '2-box'
 console.log(twoBox)
 console.log(twoBox.innerHTML)
@@ -34,10 +36,50 @@ twoBox.addEventListener('click', function() {
     console.log('The value of working string is: ' + workingString)     
 })
 
+// +
+let addBox = document.getElementById('add-box')                            // assign a value for 'add-box'
+console.log(addBox)
+console.log(addBox.innerHTML)
 
+addBox.addEventListener('click', function() {
+    console.log('Box ' + addBox.innerHTML + ' was clicked!')                  // Button + pressed
+    workingString += addBox.innerHTML
+    console.log('The value of working string is now: ' + workingString)     
+})
 
+// -
+let subBox = document.getElementById('sub-box')                            // assign a value for 'sub-box'
+console.log(subBox)
+console.log(subBox.innerHTML)
 
+subBox.addEventListener('click', function() {
+    console.log('Box ' + subBox.innerHTML + ' was clicked!')                  // Button - pressed
+    workingString += subBox.innerHTML
+    console.log('The value of working string is now: ' + workingString)     
+})
 
+// C
+let cBox = document.getElementById('C-box')                            // assign a value for 'C-box'
+console.log(cBox)
+console.log(cBox.innerHTML)
+
+cBox.addEventListener('click', function() {
+    console.log('Box ' + cBox.innerHTML + ' was clicked!')                  // Button C pressed
+    workingString = ''                                                          // workingString reset
+    console.log('The value of working string is now: ' + workingString)     
+})
+
+// E
+let eBox = document.getElementById('E-box')                            // assign a value for 'E-box'
+console.log(eBox)
+console.log(eBox.innerHTML)
+
+eBox.addEventListener('click', function() {
+    console.log('Box ' + eBox.innerHTML + ' was clicked!')                  // Button E pressed
+    displayString = workingString                                                           // Write a function to parse and evaluate  
+    console.log('The final value of the working string is : ' + workingString)     
+    console.log('This string will get convered into a value for the final display.')
+})
 
 
 

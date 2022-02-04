7b2281dc5e4c6f78540cfec6fe3a132a2e19346a
Ryan Urbanski <ryanurbanski9@gmail.com>
2022-02-03 23:44:19 -0500

    Calculator is done. Nlsneeds formatting and style.

--- main.js
+++ main.js
@@ -50,6 +50,97 @@ threeBox.addEventListener('click', function() {
     displayBox.innerHTML = workingString                                    // Update display box
 })
 
+// 4
+let fourBox = document.getElementById('4-box')                           // assign a value for '4-box' 
+console.log(fourBox)
+console.log(fourBox.innerHTML)
+
+fourBox.addEventListener('click', function() {
+    console.log('Box ' + fourBox.innerHTML + ' was clicked!')                  // Button 4 pressed
+    
+    workingString += fourBox.innerHTML
+    console.log('The value of working string is: ' + workingString)     
+    displayBox.innerHTML = workingString                                    // Update display box
+})
+
+// 5
+let fiveBox = document.getElementById('5-box')                           // assign a value for '5-box' 
+console.log(fiveBox)
+console.log(fiveBox.innerHTML)
+
+fiveBox.addEventListener('click', function() {
+    console.log('Box ' + fiveBox.innerHTML + ' was clicked!')                  // Button 5 pressed
+    
+    workingString += fiveBox.innerHTML
+    console.log('The value of working string is: ' + workingString)     
+    displayBox.innerHTML = workingString                                    // Update display box
+})
+
+// 6
+let sixBox = document.getElementById('6-box')                           // assign a value for '6-box' 
+console.log(sixBox)
+console.log(sixBox.innerHTML)
+
+sixBox.addEventListener('click', function() {
+    console.log('Box ' + sixBox.innerHTML + ' was clicked!')                  // Button 6 pressed
+    
+    workingString += sixBox.innerHTML
+    console.log('The value of working string is: ' + workingString)     
+    displayBox.innerHTML = workingString                                    // Update display box
+})
+
+// 7
+let sevenBox = document.getElementById('7-box')                           // assign a value for '7-box' 
+console.log(sevenBox)
+console.log(sevenBox.innerHTML)
+
+sevenBox.addEventListener('click', function() {
+    console.log('Box ' + sevenBox.innerHTML + ' was clicked!')                  // Button 7 pressed
+    
+    workingString += sevenBox.innerHTML
+    console.log('The value of working string is: ' + workingString)     
+    displayBox.innerHTML = workingString                                    // Update display box
+})
+
+// 8
+let eightBox = document.getElementById('8-box')                           // assign a value for '8-box' 
+console.log(eightBox)
+console.log(eightBox.innerHTML)
+
+eightBox.addEventListener('click', function() {
+    console.log('Box ' + eightBox.innerHTML + ' was clicked!')                  // Button 8 pressed
+    
+    workingString += eightBox.innerHTML
+    console.log('The value of working string is: ' + workingString)     
+    displayBox.innerHTML = workingString                                    // Update display box
+})
+
+// 9
+let nineBox = document.getElementById('9-box')                           // assign a value for '9-box' 
+console.log(nineBox)
+console.log(nineBox.innerHTML)
+
+nineBox.addEventListener('click', function() {
+    console.log('Box ' + nineBox.innerHTML + ' was clicked!')                  // Button 9 pressed
+    
+    workingString += nineBox.innerHTML
+    console.log('The value of working string is: ' + workingString)     
+    displayBox.innerHTML = workingString                                    // Update display box
+})
+
+// 0
+let zeroBox = document.getElementById('0-box')                           // assign a value for '0-box' 
+console.log(zeroBox)
+console.log(zeroBox.innerHTML)
+
+zeroBox.addEventListener('click', function() {
+    console.log('Box ' + zeroBox.innerHTML + ' was clicked!')                  // Button 0 pressed
+    
+    workingString += zeroBox.innerHTML
+    console.log('The value of working string is: ' + workingString)     
+    displayBox.innerHTML = workingString                                    // Update display box
+})
+
 // +
 let addBox = document.getElementById('add-box')                            // assign a value for 'add-box'
 console.log(addBox)
@@ -82,6 +173,18 @@ console.log(multBox.innerHTML)
 multBox.addEventListener('click', function() {
     console.log('Box ' + multBox.innerHTML + ' was clicked!')                  // Button * pressed
     workingString += multBox.innerHTML
+    console.log('The value of working string is now: ' + workingString)     
+    displayBox.innerHTML = workingString                                    // Update display box
+})
+
+// /
+let divBox = document.getElementById('div-box')                            // assign a value for 'div-box'
+console.log(divBox)
+console.log(divBox.innerHTML)
+
+divBox.addEventListener('click', function() {
+    console.log('Box ' + divBox.innerHTML + ' was clicked!')                  // Button / pressed
+    workingString += divBox.innerHTML
     console.log('The value of working string is now: ' + workingString)     
     displayBox.innerHTML = workingString                                    // Update display box
 })
